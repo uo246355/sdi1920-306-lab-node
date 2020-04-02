2a9bb69fd5f8ad6f5a512f6f7d723200e9131b85
uo246355 <uo246355@uniovi.es>
2020-04-02 15:23:02 +0200

    SDINodeJS-9.7 y 9.8- Manejo de errores en la aplicación e implementando HTTPS

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 let express = require('express');
 let app = express();
 
+let fs = require('fs');
+let https = require('https');
+
 let expressSession = require('express-session');
 app.use(expressSession({
     secret: 'abcdefg',
@@ -115,7 +118,18 @@ app.get('/', function (req, res) {
 
 // lanzar el servidor
 
-app.listen(app.get('port'), function () {
-    console.log("Servidor activo");
+app.use( function(err, req, res, next) {
+    console.log("Error producido: " + err); // mostramos el error en consola
+    if(! res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+});
 
+
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function() {
+    console.log("Servidor activo");
 });
